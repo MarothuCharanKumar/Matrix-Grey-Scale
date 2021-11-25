90fd2b7fa8a6e0a08ad0436dd7b8edd1f57b212c
MarothuCharanKumar <52210963+MarothuCharanKumar@users.noreply.github.com>
2021-11-26 04:34:02 +0530

    loop back apis

---
+++ api calls/test service/main/apiCall.py
@@ -0,0 +1,20 @@
+class Test:
+  
+  def ai(self):
+    print("Loop Executed")
+    
+  def sys(self):
+    print("root user")
+    
+  def eventHandler(self):
+    try:
+      lp = []
+      if lp[i] == 0:
+        print("exec")
+    except:
+      print("main loop started")
+def main():
+  test = Test()
+  test.ai()
+  test.sys()
+if __name__ = '__main__':main()
