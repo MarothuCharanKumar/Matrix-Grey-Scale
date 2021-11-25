39e2d5333af4b2e5a67d660cf50cff36ee252d89
MarothuCharanKumar <52210963+MarothuCharanKumar@users.noreply.github.com>
2021-11-26 04:25:27 +0530

    trsap commit

---
+++ api calls/test service/sh.py
@@ -0,0 +1,13 @@
+def test(self):
+  i = int(input())
+  j = int(input())
+
+  for in range(j):
+    l1 = []
+    l2 = []
+  for in range(i):
+    l1.append(j)
+
+if __name__=="__main__":
+  test(self)
+  
